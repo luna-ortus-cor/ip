748e308992236a3b51cebc6cb2c0e07b48583cb3
luna-ortus-cor <hentai@lunaortuscor>
2025-01-18 04:40:36 +0800

    Add Level 1

--- src/main/java/Miku.java
+++ src/main/java/Miku.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Miku{
     public static void main(String args[]){
-        System.out.println("Hello! I'm Miku!\nWhat can I do for you?\n\nBye. Hope to see you again soon!");
+        System.out.println("Hello! I'm Miku!\nWhat can I do for you?\n");
+        Scanner sc = new Scanner(System.in);
+        String in = sc.nextLine();
+        while(!in.equals("bye")){
+            System.out.println(in);
+            in=sc.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
