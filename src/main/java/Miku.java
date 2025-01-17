3e680855a6df7f58f3e29338d6ea40bd34b8610e
luna-ortus-cor <hentai@lunaortuscor>
2025-01-18 02:34:38 +0800

    Add Level 0

---
+++ src/main/java/Miku.java
@@ -0,0 +1,5 @@
+public class Miku{
+    public static void main(String args[]){
+        System.out.println("Hello! I'm Miku!\nWhat can I do for you?\n\nBye. Hope to see you again soon!");
+    }
+}
