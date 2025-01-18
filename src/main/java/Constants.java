95b4a24debf740ea970d84bdea99dc6e537a3162
luna-ortus-cor <hentai@lunaortuscor>
2025-01-19 02:42:55 +0800

    Add Level 4

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,4 +1,8 @@
 public class Constants{
     public static final String INDENT = "    ";
-    public static final String MIKU_LOGO =  "__  __ _ _\n|  \\/  (_) | ___   _\n| |\\/| | | |/ / | | |\n| |  | | |   <| |_| |\n|_|  |_|_|_|\\_\\\\__,_|";
+    public static final String MIKU_LOGO =  " __  __ _ _\n|  \\/  (_) | ___   _\n| |\\/| | | |/ / | | |\n| |  | | |   <| |_| |\n|_|  |_|_|_|\\_\\\\__,_|";
+    public static final String EXIT_MSG = "byee~ hope to see you again soon!";
+    public static final String INVALID_INSTR = ":3 oops! i don't understand that!";
+    public static final String ADD_TASK = "got it! I've added this task desu:";
+    public static final String TASK_LIST_MSG = "here are the tasks in your list desu:";
 }
--- src/main/java/Miku.java
+++ src/main/java/Miku.java
@@ -6,8 +6,8 @@ public class Miku{
 
     public static void main(String args[]){
         System.out.println(Constants.MIKU_LOGO);
-        System.out.println(Constants.INDENT+"Hello! I'm Miku!");
-        System.out.println(Constants.INDENT+"What can I do for you?");
+        System.out.println(Constants.INDENT+"hello! i'm Miku desuyo!");
+        System.out.println(Constants.INDENT+"what can i do for you?");
         Scanner sc = new Scanner(System.in);
         String in = sc.nextLine();
         while(!in.equals("bye")){
@@ -17,20 +17,43 @@ public class Miku{
                 taskList.get(Integer.valueOf(in.split(" ")[1])-1).markDone();
             }else if(in.matches("unmark \\d+")){
                 taskList.get(Integer.valueOf(in.split(" ")[1])-1).markNotDone();
-            }else{
-                Task t = new Task(in);
+            }else if(in.split(" ")[0].equals("todo")){
+                String name = in.split("todo ")[1];
+                Todo t = new Todo(name);
                 taskList.add(t);
-                System.out.println(Constants.INDENT+"added: "+in);
+                System.out.println(Constants.INDENT+Constants.ADD_TASK);
+                System.out.println(Constants.INDENT+Constants.INDENT+t.toString());
+                System.out.println(Constants.INDENT+"now you have "+taskList.size()+" tasks in the list.");
+            }else if(in.split(" ")[0].equals("deadline")){
+                String name = in.split(" /by ")[0].split("deadline ")[1];
+                String by = in.split(" /by ")[1];
+                Deadline d = new Deadline(name,by);
+                taskList.add(d);
+                System.out.println(Constants.INDENT+Constants.ADD_TASK);
+                System.out.println(Constants.INDENT+Constants.INDENT+d.toString());
+                System.out.println(Constants.INDENT+"now you have "+taskList.size()+" tasks in the list.");
+            }else if(in.split(" ")[0].equals("event")){
+                String name = in.split(" /from ")[0].split("event ")[1];
+                String from = in.split(" /from ")[1].split(" /to ")[0];
+                String to = in.split(" /from ")[1].split(" /to ")[1];
+                Event e = new Event(name,from,to);
+                taskList.add(e);
+                System.out.println(Constants.INDENT+Constants.ADD_TASK);
+                System.out.println(Constants.INDENT+Constants.INDENT+e.toString());
+                System.out.println(Constants.INDENT+"now you have "+taskList.size()+" tasks in the list.");
+            }else{
+                System.out.println(Constants.INDENT+Constants.INVALID_INSTR);
             }
             in=sc.nextLine();
         }
-        System.out.println(Constants.INDENT+"Byee~ Hope to see you again soon!");
+        System.out.println(Constants.INDENT+Constants.EXIT_MSG);
     }
 
     private static <T> void printList(ArrayList<T> list){
         int idx = 1;
+        System.out.println(Constants.INDENT+Constants.TASK_LIST_MSG);
         for(T t:list){
-            System.out.println(Constants.INDENT+idx+". "+t.toString());
+            System.out.println(Constants.INDENT+Constants.INDENT+idx+". "+t.toString());
             idx++;
         }
     }
