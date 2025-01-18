292b6e8b8bffe0d159e219e1bf957d3ccf65c599
luna-ortus-cor <hentai@lunaortuscor>
2025-01-18 15:40:28 +0800

    Add Level 2

--- src/main/java/Miku.java
+++ src/main/java/Miku.java
@@ -1,14 +1,31 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Miku{
+    private static ArrayList<Task> taskList = new ArrayList<Task>();
+
     public static void main(String args[]){
         System.out.println("Hello! I'm Miku!\nWhat can I do for you?\n");
         Scanner sc = new Scanner(System.in);
         String in = sc.nextLine();
         while(!in.equals("bye")){
-            System.out.println(in);
+            if(in.equals("list")){
+                printList(taskList);       
+            }else{
+                Task t = new Task(in);
+                taskList.add(t);
+                System.out.println("added: "+t.toString());
+            }
             in=sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static <T> void printList(ArrayList<T> list){
+        int idx = 1;
+        for(T t:list){
+            System.out.println(idx+". "+t.toString());
+            idx++;
+        }
     }
 }
