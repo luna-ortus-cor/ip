b843a45328187bc3a0c5db05d92f5ddbf6c080fe
luna-ortus-cor <hentai@lunaortuscor>
2025-01-19 21:26:51 +0800

    Add Level 5

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -2,7 +2,11 @@ public class Constants{
     public static final String INDENT = "    ";
     public static final String MIKU_LOGO =  " __  __ _ _\n|  \\/  (_) | ___   _\n| |\\/| | | |/ / | | |\n| |  | | |   <| |_| |\n|_|  |_|_|_|\\_\\\\__,_|";
     public static final String EXIT_MSG = "byee~ hope to see you again soon!";
-    public static final String INVALID_INSTR = ":3 oops! i don't understand that!";
+    public static final String INSTR_INVALID = ":3 oops! i don't understand that!";
     public static final String ADD_TASK = "got it! I've added this task desu:";
     public static final String TASK_LIST_MSG = "here are the tasks in your list desu:";
+    public static final String TODO_EMPTY_WS = ":3 oops! name field of todo cannot be empty!";
+    public static final String DEADLINE_EMPTY_WS = ":3 oops! name and/or by field of deadline cannot be empty!";
+    public static final String EVENT_EMPTY_WS = ":3 oops! name and/or from and/or to field of event cannot be empty!";
+    public static final String LIST_IDX_INVALID = ":3 oops! no such task exists or task number is empty!";
 }
--- src/main/java/Miku.java
+++ src/main/java/Miku.java
@@ -11,38 +11,20 @@ public class Miku{
         Scanner sc = new Scanner(System.in);
         String in = sc.nextLine();
         while(!in.equals("bye")){
-            if(in.equals("list")){
+            if(in.trim().equals("list")){
                 printList(taskList);       
             }else if(in.matches("mark \\d+")){
-                taskList.get(Integer.valueOf(in.split(" ")[1])-1).markDone();
+                handleMark(in,1);
             }else if(in.matches("unmark \\d+")){
-                taskList.get(Integer.valueOf(in.split(" ")[1])-1).markNotDone();
+                handleMark(in,0);
             }else if(in.split(" ")[0].equals("todo")){
-                String name = in.split("todo ")[1];
-                Todo t = new Todo(name);
-                taskList.add(t);
-                System.out.println(Constants.INDENT+Constants.ADD_TASK);
-                System.out.println(Constants.INDENT+Constants.INDENT+t.toString());
-                System.out.println(Constants.INDENT+"now you have "+taskList.size()+" tasks in the list.");
+                handleTodo(in);
             }else if(in.split(" ")[0].equals("deadline")){
-                String name = in.split(" /by ")[0].split("deadline ")[1];
-                String by = in.split(" /by ")[1];
-                Deadline d = new Deadline(name,by);
-                taskList.add(d);
-                System.out.println(Constants.INDENT+Constants.ADD_TASK);
-                System.out.println(Constants.INDENT+Constants.INDENT+d.toString());
-                System.out.println(Constants.INDENT+"now you have "+taskList.size()+" tasks in the list.");
+                handleDeadline(in);
             }else if(in.split(" ")[0].equals("event")){
-                String name = in.split(" /from ")[0].split("event ")[1];
-                String from = in.split(" /from ")[1].split(" /to ")[0];
-                String to = in.split(" /from ")[1].split(" /to ")[1];
-                Event e = new Event(name,from,to);
-                taskList.add(e);
-                System.out.println(Constants.INDENT+Constants.ADD_TASK);
-                System.out.println(Constants.INDENT+Constants.INDENT+e.toString());
-                System.out.println(Constants.INDENT+"now you have "+taskList.size()+" tasks in the list.");
+                handleEvent(in);
             }else{
-                System.out.println(Constants.INDENT+Constants.INVALID_INSTR);
+                handleError(1);
             }
             in=sc.nextLine();
         }
@@ -55,6 +37,95 @@ public class Miku{
         for(T t:list){
             System.out.println(Constants.INDENT+Constants.INDENT+idx+". "+t.toString());
             idx++;
+        }
+    }
+    
+    //mark done if mark=1, else mark not done if mark=0
+    private static void handleMark(String in, int mark){
+        try{
+            int idx = Integer.valueOf(in.split(" ")[1])-1;
+            if(mark==1){
+                taskList.get(idx).markDone();
+            }else{
+                taskList.get(idx).markNotDone();
+            }
+        }catch(IndexOutOfBoundsException e){
+            handleError(5);
+        }
+    }
+
+    private static void handleTodo(String in){
+        try{
+            String name = in.split("todo ")[1];
+            if(name.trim().isEmpty()){
+                handleError(2);
+            }else{
+                Todo t = new Todo(name);
+                taskList.add(t);
+                System.out.println(Constants.INDENT+Constants.ADD_TASK);
+                System.out.println(Constants.INDENT+Constants.INDENT+t.toString());
+                System.out.println(Constants.INDENT+"now you have "+taskList.size()+" tasks in the list.");
+            }
+        }catch(ArrayIndexOutOfBoundsException e){
+            handleError(2);
+        }
+    }
+
+    private static void handleDeadline(String in){
+        try{
+            String name = in.split(" /by ")[0].split("deadline ")[1];
+            String by = in.split(" /by ")[1];
+            if(name.trim().isEmpty() || by.trim().isEmpty()){
+                handleError(3);
+            }else{
+                Deadline d = new Deadline(name,by);
+                taskList.add(d);
+                System.out.println(Constants.INDENT+Constants.ADD_TASK);
+                System.out.println(Constants.INDENT+Constants.INDENT+d.toString());
+                System.out.println(Constants.INDENT+"now you have "+taskList.size()+" tasks in the list.");
+            }
+        }catch(ArrayIndexOutOfBoundsException e){
+            handleError(3);
+        }
+    }
+
+    private static void handleEvent(String in){
+        try{
+            String name = in.split(" /from ")[0].split("event ")[1];
+            String from = in.split(" /from ")[1].split(" /to ")[0];
+            String to = in.split(" /from ")[1].split(" /to ")[1];
+            if(name.trim().isEmpty() || from.trim().isEmpty() || to.trim().isEmpty()){
+                handleError(4);
+            }else{
+                Event e = new Event(name,from,to);
+                taskList.add(e);
+                System.out.println(Constants.INDENT+Constants.ADD_TASK);
+                System.out.println(Constants.INDENT+Constants.INDENT+e.toString());
+                System.out.println(Constants.INDENT+"now you have "+taskList.size()+" tasks in the list.");
+            }
+        }catch(ArrayIndexOutOfBoundsException e){
+            handleError(4);
+        }
+    }
+                
+    //error codes
+    //1:invalid instruction
+    //2:name field of todo is empty or whitespace
+    //3:name and/or by field of deadline is empty or whitespace
+    //4:name and/or from and/or to field of event is empty or whitespace
+    //5:no such task exists or task number is empty or whitespace
+    private static void handleError(int error){
+        switch(error){
+            case 1: System.out.println(Constants.INDENT+Constants.INSTR_INVALID);
+                    break;
+            case 2: System.out.println(Constants.INDENT+Constants.TODO_EMPTY_WS);
+                    break;
+            case 3: System.out.println(Constants.INDENT+Constants.DEADLINE_EMPTY_WS);
+                    break;
+            case 4: System.out.println(Constants.INDENT+Constants.EVENT_EMPTY_WS);
+                    break;
+            case 5: System.out.println(Constants.INDENT+Constants.LIST_IDX_INVALID);
+                    break;
         }
     }
 }
